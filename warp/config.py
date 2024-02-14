5dff139a0e6623be3f4099587b30e7b867b53de9
Léo BONNARGENT <leo.bonnargent@gmail.com>
2024-02-14 18:40:23 +0100

    Add config to omit some weekdays

--- warp/config.py
+++ warp/config.py
@@ -19,8 +19,11 @@ class DefaultSettings(object):
     OMITTED_WEEKDAYS = []
         
     # opening and closing time in seconds from 00:00
-    BOOK_OPEN = 0
-    BOOK_CLOSE = 24 * 3600
+    BOOK_OPEN = 6 * 3600
+    BOOK_CLOSE = 20 * 3600
+    # Weekdays to hide for reservation, 0 for monday to 6 for sunday
+    # Set to [5,6] to omit weekends
+    OMITTED_WEEKDAYS = [5, 6]
 
     MAX_CONTENT_LENGTH = 5 * 1024 * 1024
 
