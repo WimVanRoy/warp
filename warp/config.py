a266fa4528f632d20163437198c51ea2973ae4d2
Wim Van Roy <wim.van.roy@atlascopco.com>
2024-06-09 18:59:27 +0200

    Increase duration to three weeks.

--- warp/config.py
+++ warp/config.py
@@ -12,7 +12,7 @@ class DefaultSettings(object):
 
     # for how many weeks in advance users can book a seat
     # (after the current week)
-    WEEKS_IN_ADVANCE = 2
+    WEEKS_IN_ADVANCE = 3
 
     # Weekdays to hide for reservation, 0 for monday to 6 for sunday
     # Set to [5,6] to omit weekends
